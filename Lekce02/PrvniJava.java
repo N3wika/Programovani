f1ac4856bfdcce49b796952e4a9c1348fbf77f66
N3wika <misa.arnostova@seznam.cz>
2018-11-07 22:10:43 +0100

    Ahoj světe!

---
+++ Lekce02/PrvniJava.java
@@ -0,0 +1,7 @@
+public class PrvniJava {
+    public static void main (String[] args) {
+      System.out.println ("Ahoj, lidi!");
+      System.out.println ("Ahoj, \n\t\"lidi\"!");
+      System.out.println ("Ahoj, lidi podruhe!");
+}  
+}
