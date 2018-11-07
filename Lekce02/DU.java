45da104d53770cb801f8fa94af25ac1a96256c90
N3wika <misa.arnostova@seznam.cz>
2018-11-07 22:10:16 +0100

    Muj prvni domaci ukol v jave :)

---
+++ Lekce02/DU.java
@@ -0,0 +1,11 @@
+public class DU {
+    public static void main (String[] args) {
+      System.out.println ("._____");
+      System.out.println ("|\\    \\");
+      System.out.println ("| \\ \\\\ \\");
+      System.out.println ("|  \\    \\");
+      System.out.println ("|   \"\"\"\"\"");
+      System.out.println ("'\"\"\"\"\"");
+
+}  
+}
