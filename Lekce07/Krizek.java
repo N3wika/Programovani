3e94eb3fa5837dcdb56ae9f9555c916de59d08dc
N3wika <misa.arnostova@seznam.cz>
2018-11-11 17:35:30 +0100

    Pridani metod na malovani pruhu

--- Lekce07/Krizek.java
+++ Lekce07/Krizek.java
@@ -2,28 +2,13 @@ public class Krizek {
     public static void main(String[] args) {
         int velikost = Integer.parseInt(args[0]);
 		
-		for (int i = 0; i < velikost; ++i){
-			mezery(' ', velikost);
-			vypln('X', velikost);
-			mezery(' ', velikost);
-			System.out.println();
-		}	
-		
-		for (int j = 0; j < (2 * velikost)/3; ++j) {
-			celyRadek('X', velikost);
-			System.out.println();
-		}
-		
-		for (int k = 0; k < velikost; ++k){
-			mezery(' ', velikost);
-			vypln('X', velikost);
-			mezery(' ', velikost);
-			System.out.println();
-		}
+		svislyPruh(velikost);
+		vodorovnyPruh(velikost);
+		svislyPruh(velikost);
 		
     }
 	
-	public static void mezery (char znak, int velikost){
+	private static void mezery(char znak, int velikost){
 		int pocetMezer = velikost + velikost / 3;
 		while (pocetMezer > 0) {
 			System.out.printf("%c", znak);
@@ -31,7 +16,7 @@ public class Krizek {
 		}
 	}
 	
-	public static void vypln (char znak, int velikost){
+	private static void vypln(char znak, int velikost){
 		int pocetX = velikost;
 		while (pocetX > 0) {	
 			System.out.printf("%c", znak);
@@ -39,11 +24,27 @@ public class Krizek {
 		}
 	}
 	
-	public static void celyRadek (char znak, int velikost){
+	private static void celyRadek(char znak, int velikost){
 		int xVRadku = 3 * velikost + velikost/3 + velikost/3;
 		while (xVRadku > 0){
 			System.out.printf("%c", znak);
 			--xVRadku;
+		}
+	}
+	
+	private static void svislyPruh(int velikost){
+		for (int i = 0; i < velikost; ++i){
+			mezery(' ', velikost);
+			vypln('X', velikost);
+			mezery(' ', velikost);
+			System.out.println();
+		}	
+	}
+	
+	private static void vodorovnyPruh(int velikost) {
+		for (int j = 0; j < (2 * velikost)/3; ++j) {
+			celyRadek('X', velikost);
+			System.out.println();
 		}
 	}
 }
