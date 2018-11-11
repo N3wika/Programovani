a742afd5321195ed49bdd045840ccc50ed9e2cba
N3wika <misa.arnostova@seznam.cz>
2018-11-11 11:40:34 +0100

    Prvni funkcni verze programu.

--- Lekce07/DenVRoce.java
+++ Lekce07/DenVRoce.java
@@ -1,29 +1,39 @@
  public class DenVRoce {
     public static void main(String[] args) {
-      int rok = Integer.parseInt(args[0]);
-      int mesic = Integer.parseInt(args[1]);
-      int den = Integer.parseInt(args[2]);
+		int den = Integer.parseInt(args[0]);
+		int mesic = Integer.parseInt(args[1]);
+		int rok = Integer.parseInt(args[2]);
+	  
+		awh.IntList dnyVMesici = dny(rok);
       
-      dnyVMesici = dny(rok);
-      
-      int denVRoce = celkemDnyVMesicich + den
+		int denVRoce = celkemDnyVMesicich(dnyVMesici, mesic) + den;
+	  
+	System.out.printf("%d", denVRoce);
              
     }  
 
-   public static boolean jePrestupny(int rok) {
-        return (rok % 4) == 0;
+	public static boolean jePrestupny(int rok) {
+		return (rok % 4) == 0 && ((rok % 100) != 0 || (rok % 400) == 0);
+		
+	
    }
 
    public static awh.IntList dny(int rok) {
       if (jePrestupny(rok)) {
-        return awh.IntList.create(31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)          
+        return awh.IntList.create(31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31);          
       } else {
-        return awh.IntList.create(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)   
+        return awh.IntList.create(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31);   
       }
     }
     
-    public static void celkemDnyVMesici (){
-          int celeMesice = mesic - 1
+    public static int celkemDnyVMesicich (awh.IntList dnyVMesici, int mesic){
+		int soucetDnu = 0;
+		
+		//i < mesic - 1 => zaciname NULOU!!!
+		for (int i = 0; i < mesic - 1; i++) {
+			soucetDnu += dnyVMesici.get(i);
+		}
+		return soucetDnu;
     }
         
 }
