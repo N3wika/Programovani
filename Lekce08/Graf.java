eddf090fe7568bc6f487c05fb62333a4ae8d3933
N3wika <misa.arnostova@seznam.cz>
2018-11-20 22:50:11 +0100

    Esteticke upravy :]

--- Lekce08/Graf.java
+++ Lekce08/Graf.java
@@ -1,61 +1,63 @@
 public class Graf {
-    public static void main(String[] args) {
+
+    public static void main( String[] args ) {
+        System.out.println("Vlozte cislicka - vypadne graf :]");
         awh.DoubleList vsechnyHodnoty = awh.DoubleList.create();
-        java.util.Scanner sc = new java.util.Scanner(System.in);
-        
-        while (sc.hasNextDouble()) {
-            vsechnyHodnoty.add(sc.nextDouble());
+        java.util.Scanner sc = new java.util.Scanner( System.in );
+
+        while ( sc.hasNextDouble() ) {
+            vsechnyHodnoty.add( sc.nextDouble() );
         }
-        tiskGrafu(aplikujiMeritko(vsechnyHodnoty));
+        tiskGrafu( aplikujiMeritko( vsechnyHodnoty ) );
     }
+
+
     //uprava hodnot
-    private static awh.DoubleList aplikujiMeritko (awh.DoubleList vsechnyHodnoty){
+
+    private static awh.DoubleList aplikujiMeritko( awh.DoubleList vsechnyHodnoty ) {
         awh.DoubleList usmerneneHodnoty = awh.DoubleList.create();
-        double nejvetsi = hledamNejvetsi(vsechnyHodnoty);
-        if(nejvetsi > 60) {
-            int poradi = 0;
-            int pocetHodnot = vsechnyHodnoty.size();
-            while(pocetHodnot > 0){
-                usmerneneHodnoty.add(vsechnyHodnoty.get(poradi) * hledamMeritko(nejvetsi));
-                --pocetHodnot;
-                ++poradi;
+        double nejvetsi = hledamNejvetsi( vsechnyHodnoty );
+        if ( nejvetsi > 60.0 ) {
+            for ( int i = 0; i < vsechnyHodnoty.size(); ++i ) {
+                usmerneneHodnoty.add( vsechnyHodnoty.get( i ) * hledamMeritko( nejvetsi ) );
             }
         } else {
             usmerneneHodnoty = vsechnyHodnoty;
         }
         return usmerneneHodnoty;
     }
-    private static double hledamMeritko(double nejvetsi) {
-        double meritko = (60 / nejvetsi); 
+
+
+    private static double hledamMeritko( double nejvetsi ) {
+        double meritko = 60.0 / nejvetsi;
         return meritko;
     }
-    private static double hledamNejvetsi (awh.DoubleList vsechnyHodnoty) {
-        double nejvetsi = vsechnyHodnoty.get(0);
-        int poradi = 0;
-        for (int pocetHodnot = vsechnyHodnoty.size(); pocetHodnot > 0; --pocetHodnot) {
-            double aktualni = vsechnyHodnoty.get(poradi);
-            if (aktualni > nejvetsi) {
+
+
+    private static double hledamNejvetsi( awh.DoubleList vsechnyHodnoty ) {
+        double nejvetsi = vsechnyHodnoty.get( 0 );
+        for ( int i = 0; i < vsechnyHodnoty.size(); ++i ) {
+            double aktualni = vsechnyHodnoty.get( i );
+            if ( aktualni > nejvetsi ) {
                 nejvetsi = aktualni;
             }
-            ++poradi;
         }
         return nejvetsi;
     }
+
+
     //tisk
-    private static void tiskGrafu(awh.DoubleList usmerneneHodnoty) {
-        
-        int poradi = 0;
-        for (int pocetHodnot = usmerneneHodnoty.size(); pocetHodnot > 0; --pocetHodnot) {
-            double dalsiPolozka = usmerneneHodnoty.get(poradi);
-            tiskRadku('X', dalsiPolozka);
-            ++poradi;
+
+    private static void tiskGrafu( awh.DoubleList usmerneneHodnoty ) {
+        for ( int i = 0; i < usmerneneHodnoty.size(); ++i ) {
+            tiskRadku( 'X', usmerneneHodnoty.get( i ).intValue() );
         }
     }
-    private static void tiskRadku(char znak, double velikost){
-        while (velikost > 0) {
-            velikost = (int)velikost;
-            System.out.printf("%c", znak);
-            -- velikost;
+
+
+    private static void tiskRadku( char znak, int velikost ) {
+        for ( int i = 0; i < velikost; ++i ) {
+            System.out.printf( "%c", znak );
         }
         System.out.println();
     }
